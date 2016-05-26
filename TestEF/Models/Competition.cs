47a62af867a0e229547538819e80f6aa66cb4e22
EmmanuelNIR <enirige@cergy.itin.fr>
2016-05-26 16:04:19 +0200

    Add Player & Competition

---
+++ TestEF/Models/Competition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEF.Models
+{
+    public class Competition
+    {
+        public enum CompetitionType{
+            PARCOUR,
+            KO,
+        }
+        [Key]
+        public long Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public String Adresse { get; set; }
+
+        public CompetitionType Type { get; set; }
+    }
+}
